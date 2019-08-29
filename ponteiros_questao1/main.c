07010dd4c0dd145d4b2048defcb2d8cfd9efce1f
Reilta <54174076+reilta@users.noreply.github.com>
2019-08-28 23:46:54 -0300

    Update main.c

--- ponteiros_questao1/main.c
+++ ponteiros_questao1/main.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    /* até o momeno ponteiros foi muito util quando tratamos de alocação de "espaço"
+    /* até o momento ponteiros foi muito util quando tratamos de alocação de "espaço"
      * de memoria do computador, ajudando o pragrama ser mais rapido e agil, além
-     * consumi menos memoria ram do computador, dependendo da forma que for utilizado. */
+     * de consumir menos memoria ram do computador, dependendo da forma que for utilizado. */
     return 0;
 }
